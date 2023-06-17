c060f80962fbae9c865ad70ffc2589aca4784d7c
Jakub Vítovec <jakvitov@gmail.com>
2023-06-17 21:19:36 +0200

    Initial commit

---
+++ src/test/java/cz/jakvitov/weatherapiconsumer/WeatherApiConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.jakvitov.weatherapiconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherApiConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
